8312dd3ad5a54269647db495f94f8f5348250519
Roman Gonzalez <romanandreg@gmail.com>
2011-10-03 19:28:05 -0700

    tickets-cqrs initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject tickets-cqrs "1.0.0-SNAPSHOT"
+  :description "a cqrs ticket playground"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-time "0.3.0-SNAPSHOT"]
+                 [clj-stacktrace "0.2.1"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+                     [criterium "0.0.1-SNAPSHOT"]
+                     [clojure-source "1.2.1"]
+                     ]
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
+  :main tickets-cqrs.core)
---
+++ test/tickets_cqrs/test/core.clj
@@ -0,0 +1,6 @@
+(ns tickets-cqrs.test.core
+  (:use [tickets-cqrs.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
