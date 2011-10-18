521130f4ec8a8a8ed74f856e760951c8efe9f324
ujihisa <ujihisa@gmail.com>
2011-10-17 20:35:52 -0700

    no spaces in the end of lines

--- src/tickets_cqrs/core.clj
+++ src/tickets_cqrs/core.clj
@@ -2,35 +2,35 @@
     (:require [redis.core :as redis]))
 
 
-(def ticket-history 
+(def ticket-history
   '({:title "foo" :body "bar"}
     {:title "lark"}
-    {:assignee 'roman :body "barbar"} 
+    {:assignee 'roman :body "barbar"}
     {:priority 3}))
 
 
 (defn ticket-key [id] (str "ticket:" id))
 
-(defn save-ticket-events [ticket-id events] 
+(defn save-ticket-events [ticket-id events]
   (doall (map #(redis/rpush (ticket-key ticket-id) %) events)))
 
-(defn get-ticket-history [ticket-id] 
-  (map read-string 
+(defn get-ticket-history [ticket-id]
+  (map read-string
        (redis/lrange (ticket-key ticket-id) 0 -1)))
 
-(defn -main [] 
+(defn -main []
   (try
     (redis/with-server {:host "127.0.0.1"}
       (let [ticket-id 123
-            r-key (ticket-key ticket-id)] 
+            r-key (ticket-key ticket-id)]
         (redis/del r-key)
         (save-ticket-events ticket-id ticket-history)
-        (println (reductions conj {} 
+        (println (reductions conj {}
                    (get-ticket-history ticket-id)))))
     (catch java.net.ConnectException e
       (binding [*out* *err*]
         (println "ERROR: Please start redis server manually!"))
       (System/exit 1))))
 
-    ;(println (reductions conj {} 
+    ;(println (reductions conj {}
     ;                     (redis/lrange "ticket:123" 0 4)))))
